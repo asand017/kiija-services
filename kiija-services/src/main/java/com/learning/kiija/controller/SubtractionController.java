b31b2e89f91e3b476908adb816a340336fa4d3ef
aaron (kb) <aaron.sanders342@gmail.com>
2023-11-22 18:45:22 -0600

    api adjustment

--- kiija-services/src/main/java/com/learning/kiija/controller/SubtractionController.java
+++ kiija-services/src/main/java/com/learning/kiija/controller/SubtractionController.java
@@ -4,7 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +21,7 @@ public class SubtractionController {
     @Autowired
     private SubtractionService subtractionService;
     
-    @GetMapping("/arithmetic/subtraction")
+    @PostMapping("/subjects/arithmetic/subtraction")
     public ResponseEntity<AdditionSubtractionResponseBean> getSubtractionProblems(@RequestBody AdditionSubtractionRequestBean request) throws ZeroDigitException {
         LOGGER.info("Request for subtraction problem set received.");
         AdditionSubtractionResponseBean response = subtractionService.generateProblems(request);
